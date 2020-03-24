835584e60488453176848c658da22f6448be96f4
Paul_Schmidt-Barbo <paul.schmibo@gmail.com>
2020-03-24 16:16:38 +0100

    further analysis, plot distribution for clusters, zoom in plots, cluster sorted plotting

--- analysis.py
+++ analysis.py
@@ -1,29 +1,50 @@
 import numpy as np
 from spectral_clustering import spectral_clustering
+import functions_for_plotting
+import matplotlib.pyplot as plt
 
 data_dir = "data/"
 
 data = np.load(data_dir + "F_signal_noise.npy")
-#np.random.shuffle(data)
+true_labels = np.repeat(range(12), 1000)
+
+seed = np.random.seed(42)
+training_split = list(range(12))
+np.random.shuffle(training_split)
+training_split = np.sort(training_split[:6])
+
+training_set_indices = []
+for i in training_split:
+    training_set_indices += list(range(i*1000,(i+1)*1000))
+training_set_indices = np.asarray(training_set_indices)
+
+training_set = data[training_set_indices]
+
 n_clusters = range(1,21)
 
-#labels, eigvec, eigval = spectral_clustering(data, "euclidean",n_clusters,  k=10, mutual = False, weighting = "distance", normalize = True, reg_lambda = 0.01, save_laplacian = False, save_eigenvalues_and_vectors = False)
+labels, eigvec, eigval = spectral_clustering(training_set, "euclidean",n_clusters,  k=10, mutual = False, weighting = "distance", normalize = True, reg_lambda = 0.1, save_laplacian = False, save_eigenvalues_and_vectors = False)
 
-
-labels = np.load("Toy_data/labels/labels_k=10_reg=0.1.npy")
+#np.save("Toy_data/labels/labels_k_10_reg=0.1_training",labels)
+labels = np.load("Toy_data/Labels/labels_k_5_reg=0.1_training.npy")
 
 
 # check if all cluster captured correctly
-labels_12 = labels[11]
+labels_6 = labels[5]
 
 
-for i in range(12):
-    burst_indices = np.where(labels_12 == i)[0]
+for i in range(6):
+    burst_indices = np.where(labels_6 == i)[0]
     wrongly_clustered_data = len(np.where(np.diff(burst_indices) > 1)[0])
     print("Cluster %d: %d wrongly clustered data points!" % ((i+1),wrongly_clustered_data))
 
 
+_, idx = np.unique(labels_12, return_index=True)
+arrangement = labels_12[np.sort(idx)]
 
-plot_cluster_examples(data, labels_12, 12,3,4, figsize = (30,25), burst_percent = False, n_bursts=50, y_lim = (0,16),plot_mean=True)
+
+functions_for_plotting.plot_cluster_examples(training_set, labels_6, 6, 2, 4, figsize = (30,25), burst_percent = False, n_bursts=None, y_lim = (0,16),plot_mean=False, arrangement = None, title = None)
+functions_for_plotting.plot_cluster_distribution(data, true_labels, 12, 3, 4,figsize = (30,25), y_lim = None, arrangement = None)
 plt.close()
+
+
 
--- functions_for_plotting.py
+++ functions_for_plotting.py
@@ -5,7 +5,7 @@ from matplotlib import pyplot as plt
 from matplotlib import cm
 import seaborn as sns
 
-def plot_cluster_examples(data, labels, k_clusters,rows,columns, figsize = (30,25), burst_percent = False, n_bursts=None, y_lim = None, plot_mean = False):
+def plot_cluster_examples(data, labels, k_clusters,rows,columns, figsize = (30,25), burst_percent = False, n_bursts=None, y_lim = None, plot_mean = False, arrangement = None, title = None):
     if k_clusters < 10:
         colors = ["C" + str(i) for i in range(k_clusters)]
     else:
@@ -13,15 +13,16 @@ def plot_cluster_examples(data, labels, k_clusters,rows,columns, figsize = (30,2
 
     fig = plt.figure(figsize=figsize)
     fig.subplots_adjust(hspace=0.4, wspace=0.4)
+    if title:
+        fig.suptitle(title, fontsize=16)
 
-    rows = rows
     for i in range(k_clusters):
-        class_i = data[np.where(labels == i)]
+        if arrangement is None:
+            class_i = data[np.where(labels == i)]
+        else:
+            class_i = data[np.where(labels == arrangement[i])]
         #class_i = np.random.permutation(class_i)
-
-        if i < k_clusters:
-            count = i + 1
-        ax = fig.add_subplot(rows, columns, count)
+        ax = fig.add_subplot(rows, columns, i+1)
         ax.set_xlabel("Time", fontsize = 12)
         if plot_mean:
             if burst_percent:
@@ -50,15 +51,100 @@ def plot_cluster_examples(data, labels, k_clusters,rows,columns, figsize = (30,2
 
 
 
-def plot_cluster_distribution(data, labels, k_clusters,rows,columns, figsize = (30,25), y_lim = None):
+def plot_cluster_distribution(data, labels, k_clusters,rows, columns, figsize = (30,25), y_lim = None, arrangement = None):
     fig = plt.figure(figsize=figsize)
+
     fig.subplots_adjust(hspace=0.4, wspace=0.4)
+
     for i in range(k_clusters):
-        if i < k_clusters:
-            count = i + 1
-        ax = fig.add_subplot(rows, columns, count)
-        class_i = data[np.where(labels == i)]
+        if arrangement is None:
+            class_i = data[np.where(labels == i)]
+        else:
+            class_i = data[np.where(labels == arrangement[i])]
+
+        if i > 0:
+            ax = fig.add_subplot(rows, columns, i + 1, sharey=ax)
+        else:
+            ax = fig.add_subplot(rows, columns, i + 1)
+            if y_lim:
+                ax.set_ylim(y_lim)
+
         sns.distplot(np.concatenate(class_i, axis=0), ax=ax)
 
+        if i > 3:
+            ax_inset = ax.inset_axes([0.5, 0.5, 0.47, 0.47])
+            sns.distplot(np.concatenate(class_i, axis=0), ax=ax_inset)
+            if i < 8:
+                x1, x2, y1, y2 = -0.2, 2, 0, 3
+            else:
+                x1, x2, y1, y2 = -0.2, 5, 0, 2.5
+            ax_inset.set_xlim(x1, x2)
+            ax_inset.set_ylim(y1, y2)
+
+            ax.indicate_inset_zoom(ax_inset)
+
+
+
+def plot_cluster_distribution_comparison(data, labels, k_clusters, figsize = (30,25), y_lim = None, arrangement = None):
+    fig = plt.figure(figsize=figsize)
+    fig.subplots_adjust(hspace=0.4, wspace=0.4)
+
+    ax1 = fig.add_subplot(1, 4, 1)
+    ax2 = fig.add_subplot(1, 4, 2)
+    ax3 = fig.add_subplot(1, 4, 3)
+    ax4 = fig.add_subplot(1, 4, 4)
+
+    ax1_inset = ax1.inset_axes([0.5, 0.5, 0.47, 0.47])
+    ax2_inset = ax2.inset_axes([0.5, 0.5, 0.47, 0.47])
+    ax3_inset = ax3.inset_axes([0.5, 0.5, 0.47, 0.47])
+    ax4_inset = ax4.inset_axes([0.5, 0.5, 0.47, 0.47])
+
+    for i in range(k_clusters):
+        if arrangement is None:
+            class_i = data[np.where(labels == i)]
+        else:
+            class_i = data[np.where(labels == arrangement[i])]
+
+        if i%4 == 0:
+            sns.distplot(np.concatenate(class_i, axis=0), ax=ax1, norm_hist=True)
+            sns.distplot(np.concatenate(class_i, axis=0), ax=ax1_inset)
+        elif i%4 == 1:
+            sns.distplot(np.concatenate(class_i, axis=0), ax=ax2, norm_hist=True)
+            sns.distplot(np.concatenate(class_i, axis=0), ax=ax2_inset)
+        elif i%4 == 2:
+            sns.distplot(np.concatenate(class_i, axis=0), ax=ax3, norm_hist=True)
+            sns.distplot(np.concatenate(class_i, axis=0), ax=ax3_inset)
+        else:
+            sns.distplot(np.concatenate(class_i, axis=0), ax=ax4, norm_hist=True)
+            sns.distplot(np.concatenate(class_i, axis=0), ax=ax4_inset)
+
         if y_lim:
-            ax.set_ylim(y_lim)
+            ax1.set_ylim(y_lim)
+            ax2.set_ylim(y_lim)
+            ax3.set_ylim(y_lim)
+            ax4.set_ylim(y_lim)
+
+
+
+        x1, x2, y1, y2 = -0.2, 3, 0, 6
+        ax1_inset.set_xlim(x1, x2)
+        ax1_inset.set_ylim(y1, y2)
+
+        x1, x2, y1, y2 = -0.2, 8, 0, 2
+        ax2_inset.set_xlim(x1, x2)
+        ax2_inset.set_ylim(y1, y2)
+
+        x1, x2, y1, y2 = -0.2, 2, 0, 2.5
+        ax3_inset.set_xlim(x1, x2)
+        ax3_inset.set_ylim(y1, y2)
+
+        x1, x2, y1, y2 = -0.2, 2, 0, 2.5
+        ax4_inset.set_xlim(x1, x2)
+        ax4_inset.set_ylim(y1, y2)
+
+
+
+        ax1.indicate_inset_zoom(ax1_inset)
+        ax2.indicate_inset_zoom(ax2_inset)
+        ax3.indicate_inset_zoom(ax3_inset)
+        ax4.indicate_inset_zoom(ax4_inset)
